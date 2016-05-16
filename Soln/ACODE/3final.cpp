7643ea72d0b499b5f9416121f1fc96031686d03e
Mudit <srivastava.mudit2908@gmail.com>
2016-05-16 23:44:34 +0530

    Few correction

--- Soln/ACODE/3final.cpp
+++ Soln/ACODE/3final.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <cstdio>
+#include <climits>
+#define ninf INT_MIN;
 using namespace std;
-int size(int z )
+int size(long int z )
 {
 	int i=0;
 	while(z>9)
@@ -12,39 +15,46 @@ int size(int z )
 	return i+1;
 
 }
-int ls(int n, vector<int> &v ,int i)
+int ls(long int n, vector<int> &v ,int i)
 {
 	int a,c;
+	int flag=1;
 	a=n%100;
 	c=n%10;
 	if(v[i]!=0) return v[i];
 
 	if(i==1)
-	{
-		if(a>26) v[1]=1;
+	{ 
+		if(a<10) v[1]=ninf;
 		else
-		 v[1]=2;
+{	
+	if(a>26) { if(c==0) v[1]=ninf; else v[1]=1;}
+		else
+		{if(c==0) v[1]=1; else v[1]=2; }
+}
 	}
 	else
 	{
-		if(a>26) v[i]=ls((n-c)/10,v,i-1);
-		else v[i]=ls((n-a)/100,v,i-2) + ls((n-c)/10,v,i-1);
+		if(a>26) { if(c==0) v[i]= ninf; else v[i]=ls((n-c)/10,v,i-1)};
+		else {if(c==0) v[i]=ls((n-a)/100,v,i-2); else v[i]=ls((n-a)/100,v,i-2) + ls((n-c)/10,v,i-1)};
 	}
-return v[i];
+
+return v[i]; 
 }
 int main()
 {
-int n=1;
+long long int n=1;
 int b=0;
 vector<int> ans;
-vector<int> ip;
+vector<long long int> ip;
 while(n!=0)
 {
-	cin>>n;
+	scanf("%lld",&n);
 	if(n) ip.push_back(n);
 }
 int i=0;
-while( ip[i]!=0)
+int j=ip.size();
+while(i<j)
 {
         n=ip[i];i++;
 	
@@ -56,11 +66,13 @@ int i;
 	{
 		v[i]=ls(n,v,i);
 	}
-	cout<<v[i];
-	ans.push_back(v[b-1]);		 
+	if(v[b-1]>0)
+	ans.push_back(v[b-1]);	
+	else
+	ans.push_back(0);	 
 
 
 }
-for(int j=0;j<ans.size();j++) cout<<ans[j]<<"\n";
+for(j=0;j<ans.size();j++) cout<<ans[j]<<"\n";
 return 0;
 }
